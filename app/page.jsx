b5edc39e53d835866a562fbc95b2368787eee0e9
Jordan <jordanroberts333@icloud.com>
2023-02-17 09:55:43 +0000

    courses images

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
 'use client'
 import { useEffect, useRef } from "react"
+import Image from "next/image"
+import mathsImage from '../images/mathss.jpg'
+import cloudImage from '../images/cloudd.jpg'
+import frontEndImage from '../images/frontEnd.jpg'
+import securityImage from '../images/security.jpg'
+import appImage from '../images/app.jpg'
+import rustImage from '../images/rust.jpg'
 
 export default function Home() {
   let originalPosition = useRef()
@@ -67,27 +74,28 @@ export default function Home() {
   return (
     <>
       <main onTouchStart={e => swipeStart(e)} onTouchMove={e => swipeMove(e)} onTouchEnd={swipeEnd}>
-        <section ref={overviewElement} id='introduction' className="h-[100svh] w-full bg-green-200">
-
+        <section ref={overviewElement} id='introduction' className="h-[100svh] w-full bg-green-200 relative">
+    
         </section>
         <section ref={coursesListElement} id='courses' className="h-[100lvh] w-full bg-green-300 overflow-y-scroll snap-y snap-mandatory">
-          <div ref={firstCourse} id='course_1' className="h-[100lvh] w-full bg-purple-100 snap-start">
-            hehe
+          <div ref={firstCourse} id='course_1' className="h-[100lvh] w-full bg-purple-100 snap-start relative">
+            <Image fill alt="brb" src={cloudImage} className='object-cover'/>
+            <h3 className="absolute top-[30%] left-[50%] translate-x-[-50%] text-white text-3xl">Cloud Computing</h3>
           </div>
-          <div id='course_2' className="h-[100lvh] w-full bg-purple-200 snap-start">
-
+          <div id='course_2' className="h-[100lvh] w-full bg-purple-200 snap-start relative">
+            <Image fill alt="brb" src={mathsImage} className='object-cover'/>
           </div>
-          <div id='course_3' className="h-[100lvh] w-full bg-purple-300 snap-start">
-
+          <div id='course_3' className="h-[100lvh] w-full bg-purple-300 snap-start relative">
+          <Image fill alt="brb" src={frontEndImage} className='object-cover'/>
           </div>
-          <div id='course_4' className="h-[100lvh] w-full bg-purple-400 snap-start">
-
+          <div id='course_4' className="h-[100lvh] w-full bg-purple-400 snap-start relative">
+          <Image fill alt="brb" src={securityImage} className='object-cover'/>
           </div>
-          <div id='course_5' className="h-[100lvh] w-full bg-purple-500 snap-start" onTouchStart={() => {trigger.current = true}}>
-
+          <div id='course_5' className="h-[100lvh] w-full bg-purple-500 snap-start relative" onTouchStart={() => {trigger.current = true}}>
+          <Image fill alt="brb" src={appImage} className='object-cover'/>
           </div>
-          <div id='course_6' className="h-[100lvh] w-full bg-purple-600 snap-start" onTouchStart={() => {coursesListElement.current.style.overflow = 'hidden'; document.body.style.overflow = 'hidden'}}>
-
+          <div id='course_6' className="h-[100lvh] w-full bg-purple-600 snap-start relative">
+          <Image fill alt="brb" src={rustImage} className='object-cover'/>
           </div>
         </section>
       </main>
